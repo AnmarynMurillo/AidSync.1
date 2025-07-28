18b93254182e66d19dcc65968ea928be287b210a
elinanp11 <elinanicoleperez@gmail.com>
2025-07-28 08:25:20 -0500

    Hze cambios

--- public/assets/js/volunteer.js
+++ public/assets/js/volunteer.js
@@ -5,7 +5,7 @@ const VOLUNTEERS = [
   {
     id: 1,
     nombre: 'Casa Esperanza/ tutoring',
-    categoria: 'Education',
+    categoria: 'educacion',
     color: '#2563eb',
     colorHover: '#1d4ed8',
     img: '/public/assets/images/volunteer/casaEsperanza.jpg',
@@ -16,7 +16,7 @@ const VOLUNTEERS = [
     src="https://www.openstreetmap.org/export/embed.html?bbox=-80.5381%2C8.2319%2C-80.5355%2C8.2338&amp;layer=mapnik&amp;marker=8.23285%2C-80.53681"  style="border:1px solid black"></iframe>`,
     requisitos: ['Over 16 years old',
     ' Knowledge in the subjects of: Spanish, mathematics, English, physics, chemistry and accounting.',
-    ' Be responsible. committed and punctual.','- Reside in the provinces of Panama, Coclé, Colón, Herrera and Chiriquí.',
+    ' Be responsible. committed and punctual.','Reside in the provinces of Panama, Coclé, Colón, Herrera and Chiriquí.',
     'Have good management of a group of children from 6 to 11 years old and adolescents from 12 to 17 years old.'],
   },
   {
@@ -28,9 +28,20 @@ const VOLUNTEERS = [
     img: '/public/assets/images/volunteer/Brigadas.jpg',
     desc: 'Global Brigades Panama is an international organization that connects volunteers with vulnerable communities to provide medical, dental, and health education services through short-term brigades.',
     descLarga: 'Become a volunteer and help bring vital healthcare and education to communities in need.',
-    ubicacion: 'Guadalajara, Jalisco',
+    ubicacion: '1003 Calle 60 Oeste, Panamá, Provincia de Panamá, PA',
     mapa:'<iframe width="100%" height="180" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-79.5377%2C8.9903%2C-79.5327%2C8.9933&amp;layer=mapnik&amp;marker=8.991856529315962%2C-79.53523216208009"  style="border:1px solid black"></iframe>',
-    requisitos: ['Mayoría de edad', 'Experiencia en salud (deseable)', 'Actitud de servicio']
+    requisitos: ['Valid passport (at least 6 months before expiration)',
+
+    'Must be 18+ years old (or have parental permission)',
+    'Fundraise between $2,440 and $2,640 USD',
+    'Availability for 7 consecutive days',
+    'Attend pre-trip orientation sessions', 
+    'Basic Spanish proficiency', 
+    'Physical ability for community work',
+    'Respect for local cultural norms',
+    'Adaptability to rural conditions', 
+    'No criminal record', 
+    'Willingness to work as part of a team']
   },
   {
     id: 3,
@@ -41,7 +52,7 @@ const VOLUNTEERS = [
     img:'/public/assets/images/volunteer/Reforestacion.jpeg',
     desc: 'Join as a volunteer for reforestation events or school/community campaigns..',
     descLarga: ' It is a nonprofit working in Bahía Piñas, Darién, to support reforestation, education, and community health in one of the most remote areas of Panama.',
-    ubicacion: 'Monterrey, Nuevo León',
+    ubicacion: 'Llanos de Curundu, Edificio 1992 A-B, Ancón, Ciudad de Panamá, Panamá.',
     mapa:'<iframe width="100%" height="180" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-79.5513%2C8.9785%2C-79.5473%2C8.9815&amp;layer=mapnik&amp;marker=8.980028423107049%2C-79.54932847557309" style="border:1px solid black"></iframe>',
     requisitos: ['Commitment to the environmental cause','Availability of time',
     'Physical ability','Minimum age (18 years or younger with authorization)','Teamwork and positive attitude', 'Participation in informational or orientation sessions', 'Compliance with safety protocols'],
@@ -53,7 +64,7 @@ const VOLUNTEERS = [
     color: '#eab308',
     colorHover: '#ca8a04',
     img:  '/public/assets/images/volunteer/Bnaco.webp',
-    desc: ' is a non-profit organization that combats hunger and malnutrition by rescuing surplus food and distributing it to vulnerable communities throughout Panama.',
+    desc: ' It is a non-profit organization that combats hunger and malnutrition by rescuing surplus food and distributing it to vulnerable communities throughout Panama.',
     descLarga: 'Join us as a volunteer and support the collection, sorting, and delivery of food to those in need.',
     ubicacion: 'Vía Circunvalación, Las Mañanitas, Tocumen, Panamá',
     mapa: '<iframe width="100%" height="180" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"  src="https://www.openstreetmap.org/export/embed.html?bbox=-79.3999%2C9.0873%2C-79.3959%2C9.0903&layer=mapnik&marker=9.088846170013202%2C-79.3978560319548"  style="border:1px solid black"></iframe>',
